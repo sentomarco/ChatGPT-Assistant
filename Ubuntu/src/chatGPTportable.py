64ab289f4c2576d78b9aaf5f215a90fe6d6b37a1
Marco Sento <70527145+sentomarco@users.noreply.github.com>
2023-04-03 21:34:24 +0200

    Add files via upload

---
+++ Ubuntu/src/chatGPTportable.py
@@ -0,0 +1,14 @@
+import subprocess, os
+
+src_PATH="./src/"
+
+# script da eseguire
+script_cmd = ['python', src_PATH+'RUN.py']
+
+# Esecuzione dello script
+os.system("python " +src_PATH+"RUN.py")
+
+
+
+
+
